38fd7d0c79c73537f0138804e43499c85594fb19
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-02-22 17:56:36 +0300

    Global Exception Handler

---
+++ N.API/Middllewares/UseCustomExceptionHandlerHelpers.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using N.Core.DTOs;
+using N.Service.Exceptions;
+using System.Text.Json;
+
+internal static class UseCustomExceptionHandlerHelpers
+{
+
+    public static void UserCustomException(this IApplicationBuilder app)
+    {
+        app.UseExceptionHandler(config =>
+        {
+
+            config.Run(async context =>
+            {
+                context.Response.ContentType = "application/json";
+
+                var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                var statusCode = exceptionFeature.Error switch
+                {
+                    ClientSideException => 400,
+                    NotFoundExcepiton => 404,
+                    _ => 500
+                };
+                context.Response.StatusCode = statusCode;
+
+
+                var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+
+            });
+
+        });
+    }
+}
--- N.API/Program.cs
+++ N.API/Program.cs
@@ -65,6 +65,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UserCustomException();
+
 app.UseAuthorization();
 
 app.MapControllers();
